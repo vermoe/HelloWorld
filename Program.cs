c1988af40efeea2efa888fee5d28c338a19f0aec
Vera Moesgaard <Vera@MacBook-Air.local>
2025-02-03 20:39:17 +0100

    second commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
 ﻿namespace HelloWorld;
 
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
     }
 }
