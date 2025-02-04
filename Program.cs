ba23ccfde58c4f99b933797c85d84fd4d5553907
Vera Moesgaard <veramoesgaard2007@gmail.com>
2025-02-04 09:44:28 +0100

    third commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
 ﻿namespace HelloWorld;
 
+// source: Pro C# 10 book, chapter 5
+
 class Program {
-    static void Main(string[] args) {
-        Console.WriteLine("Hello, Beautiful World!");
-    }
+    public string programName;
+    public int swagLevel;
+
+    public void PrintState()
+        => Console.WriteLine("{0} has a swaglevel of {1}!", programName, swagLevel);
+
+    public void increaseSwag(int swag)
+        => swagLevel += swag    
 }
