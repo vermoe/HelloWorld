f2b102355020e9fe7488e0c4cb76b933978bc21d
Vera <veramoesgaard2007@gmail.com>
2025-03-17 13:30:25 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,5 +10,5 @@ class Program {
         => Console.WriteLine("Hello! {0} has a swaglevel of {1}!", programName, swagLevel);
 
     public void increaseSwag(int swag)
-        => swagLevel += swag    
+        => swagLevel += swag;
 }
